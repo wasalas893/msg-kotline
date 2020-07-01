442f0fd8cc1bc14ac652d5b1734a8ea7a996e873
wasalas <wasalas893@gmail.com>
2020-07-01 16:32:54 +0530

    create the signOut

--- app/src/main/java/com/example/msg/LatesMessagesActivity.kt
+++ app/src/main/java/com/example/msg/LatesMessagesActivity.kt
@@ -1,12 +1,57 @@
 package com.example.msg
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
 
 class LatesMessagesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lates_messages)
+
+        //singout firebase method
+
+       verifyUserIsLoogedIn()
+
+
+
+
+
+
     }
+    private  fun verifyUserIsLoogedIn(){
+        val uid=FirebaseAuth.getInstance().uid
+        if(uid==null){
+            val intent=Intent(this,MainActivity::class.java)
+            intent.flags=Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+            R.id.menu_new_message->{
+
+            }
+            R.id.menu_sign_out->{
+                FirebaseAuth.getInstance().signOut()
+                val intent=Intent(this,MainActivity::class.java)
+                intent.flags=Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
 }
